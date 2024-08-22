6e1a23ca3a521f24130e25d4c58700c8572015ea
Adeesha <waadissanayake@gmail.com>
2024-08-23 01:56:08 +0530

    Home

--- opal-system/src/app/page.tsx
+++ opal-system/src/app/page.tsx
@@ -1,8 +1,38 @@
 export default function SearchPage() {
-  return(
+
+  return (
     <div>
-      <div>Check-In/Check-out Dates</div>
-      <div>Guest Count</div>
+      <div className='flex flex-row justify-between p-10 border border-black m-10 rounded'>
+        <div className="flex flex-col">
+          <label htmlFor="check-in">Check-In</label>
+          <input type="date" id='check-in' />
+        </div>
+        <div className="flex flex-col">
+          <label htmlFor="check-out">Check-Out</label>
+          <input type="date" id='check-out' />
+        </div>
+        <div className="align-middle">Guest Count</div>
+        <button>Search</button>
+      </div>
+      <div className="p-10">
+        <div className="flex mt-10 mb-10">
+          <div className="border border-black w-40 h-40 mr-10"></div>
+          <div className="flex flex-col justify-between">
+            <h1>Hotel 1</h1>
+            <p>description</p>
+            <h1>$100</h1>
+          </div>
+        </div>
+        <div className=" flex mt-10 mb-10">
+          <div className="border border-black w-40 h-40 mr-10"></div>
+          <div className="flex flex-col justify-between">
+            <h1>Hotel 1</h1>
+            <p>description</p>
+            <h1>$100</h1>
+          </div>
+        </div>
+      </div>
     </div>
+
   )
 }
