92b71dfa521ed20b8b3118c8b7e9704affea9cf2
Harshana-2000 <lakshara.21@cse.mrt.ac.lk>
2024-09-14 18:57:41 +0530

    update

--- script.groovy
+++ script.groovy
@@ -13,7 +13,7 @@ def versionIncrement() {
     def newVersion = "${major}.${minor}.${newPatch}"
 
     // Update package.json with the new version
-    sh "npm version ${newVersion} --no-git-tag-version"
+    bat "npm version ${newVersion} --no-git-tag-version"
 
     echo "New Image Version Tag is ${newVersion}-${BUILD_NUMBER}"
 
@@ -21,29 +21,26 @@ def versionIncrement() {
     return "${newVersion}-${BUILD_NUMBER}"
 }
 
-
 def deploy() {
     echo "Deploying the app to EC2 instance"
 
     def dockerCommand = "docker run -d -p 3000:3000 yourdockerhubusername/your-app-name:${env.IMAGE_TAG}"
 
     sshagent(['aws-ec2-ssh']) {
-        sh "ssh -o StrictHostKeyChecking=no ec2-user@your-ec2-ip-address ${dockerCommand}"
+        // Execute SSH command for Windows
+        bat "ssh -o StrictHostKeyChecking=no ec2-user@your-ec2-ip-address ${dockerCommand}"
     }
 }
 
 def commitVersionUpdate() {
     sshagent(['jenkins-github-ssh']) {
-        sh 'git config user.email "jenkins@ci-cd.com"'
-        sh 'git config user.name "Jenkins User"'
-        sh "git remote set-url origin git@github.com:YourUsername/your-repo-name.git"
-        sh "git add ."
-        sh 'git commit -m "Jenkins Version Bump"'
-        sh 'git push origin HEAD:main'
+        bat 'git config user.email "jenkins@ci-cd.com"'
+        bat 'git config user.name "Jenkins User"'
+        bat "git remote set-url origin git@github.com:YourUsername/your-repo-name.git"
+        bat "git add ."
+        bat 'git commit -m "Jenkins Version Bump"'
+        bat 'git push origin HEAD:main'
     }
 }
 
 return this
-
-
-
